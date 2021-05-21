585f79b45299b09b7e45ed4ab0977be333b82a3d
vlapwd <iyu@vlapwd.com>
2021-05-21 16:15:35 +0900

    + util

--- src/main/java/com/example/demo/service/HogeSevice.java
+++ src/main/java/com/example/demo/service/HogeSevice.java
@@ -1,11 +1,17 @@
 package com.example.demo.service;
 
+import com.example.demo.util.HogeUtil;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HogeSevice {
+    private final HogeUtil hogeUtil;
+
+    public HogeSevice(HogeUtil hogeUtil) {
+        this.hogeUtil = hogeUtil;
+    }
 
     public void simple(){
-        System.out.println("simple");
+        System.out.println(hogeUtil.SIMPLE_STR);
     }
 }
---
+++ src/main/java/com/example/demo/util/HogeUtil.java
@@ -0,0 +1,8 @@
+package com.example.demo.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HogeUtil {
+    public final String SIMPLE_STR = "hoge";
+}
